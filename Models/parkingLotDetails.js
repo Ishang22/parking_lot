b20c532faa1a73e7f28ce5a653ae486c33e4d10e
ishangarg <ishangarg@policybazaar.com>
2020-01-02 13:33:21 +0530

    up

--- Models/parkingLotDetails.js
+++ Models/parkingLotDetails.js
@@ -1,13 +1,17 @@
 let setSlotLimit = require("./setSlotLimit");
 
 let ParkingLotDetails = class extends setSlotLimit {
-  constructor(vehicleNumber, vehicleColour, SlotLimit, initialLimit) {
-    //public variable
-    super(SlotLimit, initialLimit);
-    this.vehicleNumber = vehicleNumber;
-    this.vehicleColour = vehicleColour;
-    this.isSlotAlloted = false;
+  constructor(SlotLimit, initialLimit, carDetails) {
+    super(SlotLimit, initialLimit, carDetails);
   }
+
+  getStatus() {}
+
+  leaveSlot() {}
+
+  getRegistrationNumberWithColor() {}
+
+  getSlotNumberWithColor() {}
 };
 
 module.exports = ParkingLotDetails;
--- index.js
+++ index.js
@@ -7,24 +7,28 @@ standard_input.setEncoding("utf-8");
 console.log("Please input text in command line.");
 
 standard_input.on("data", async data => {
-  let expr = data.split(" ")[0];
-  switch (expr) {
+  data = data.split(" ");
+  data[data.length - 1] = data[data.length - 1].replace(/\n|\r/g, "");
+  switch (data[0]) {
     case "create_parking_lot":
-      await parkingLotController.createParkingLot(data.split(" "));
+      await parkingLotController.createParkingLot(data);
       break;
     case "park":
-      await parkingLotController.allotParkingSlot(data.split(" "));
+      await parkingLotController.allotParkingSlot(data);
       break;
     case "leave":
-      await parkingLotController.leaveSlot(data.split(" "));
+      await parkingLotController.leaveSlot(data);
       break;
     case "status":
+      await parkingLotController.getStatusOfCar(data);
       break;
     case "registration_numbers_for_cars_with_colour":
+      await parkingLotController.getNumbersWithColour(data);
       break;
     case "slot_numbers_for_cars_with_colour":
+      await parkingLotController.leaveSlot(data);
       break;
-    case "exit\n":
+    case "exit":
       fs.unlink(fileName, err => {
         if (err) throw err;
         process.exit();
