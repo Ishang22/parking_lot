84f5887206abff47b8a93a7cb71f38ec8e149901
ishangarg <ishangarg@policybazaar.com>
2020-01-02 23:14:26 +0530

    update

--- Models/parkingLotDetails.js
+++ Models/parkingLotDetails.js
@@ -4,14 +4,6 @@ let ParkingLotDetails = class extends setSlotLimit {
   constructor(SlotLimit, initialLimit, carDetails) {
     super(SlotLimit, initialLimit, carDetails);
   }
-
-  getStatus() {}
-
-  leaveSlot() {}
-
-  getRegistrationNumberWithColor() {}
-
-  getSlotNumberWithColor() {}
 };
 
 module.exports = ParkingLotDetails;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let parkingLotController = require("./Controllers/index").parkingLotController;
+let keyMapFunction = require("./Constant/keyToFile");
 let standard_input = process.stdin;
 const fs = require("fs");
 const fileName = "groceryList.json";
@@ -9,33 +10,12 @@ console.log("Please input text in command line.");
 standard_input.on("data", async data => {
   data = data.split(" ");
   data[data.length - 1] = data[data.length - 1].replace(/\n|\r/g, "");
-  switch (data[0]) {
-    case "create_parking_lot":
-      await parkingLotController.createParkingLot(data);
-      break;
-    case "park":
-      await parkingLotController.allotParkingSlot(data);
-      break;
-    case "leave":
-      await parkingLotController.leaveSlot(data);
-      break;
-    case "status":
-      await parkingLotController.getStatusOfCar(data);
-      break;
-    case "registration_numbers_for_cars_with_colour":
-      await parkingLotController.getNumbersWithColour(data);
-      break;
-    case "slot_numbers_for_cars_with_colour":
-      await parkingLotController.leaveSlot(data);
-      break;
-    case "exit":
-      fs.unlink(fileName, err => {
-        if (err) throw err;
-        process.exit();
-      });
-      process.exit();
-      break;
-    default:
-      console.log("invalid input");
+
+  if (keyMapFunction[data[0]]) {
+    await parkingLotController[keyMapFunction[data[0]]](data);
+  } else if (data[0].includes(".txt")) {
+    await parkingLotController.readDataFromFile(data);
+  } else {
+    console.log("invalid input");
   }
 });
